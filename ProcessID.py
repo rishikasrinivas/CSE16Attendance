4e0f25ee94abb2eab3d416eb5f1a707173eb3c72
rishikasrinivas <04rishikasrinivas@gmail.com>
2023-04-21 20:02:20 -0700

    Records fist and last

--- ProcessID.py
+++ ProcessID.py
@@ -22,14 +22,29 @@ class ProcessID:
             update = f1.readlines()
             for i in update:
                 if countClass > 0:
-                    classList.append(i.strip().split(",")[2])
+                    classList.append(i.strip().split(",")[0])
                 countClass += 1
 
         for i in classList:
             if i not in loggedIn:
                 absent.append(i)
+        return self.getNames(classRoster, absent)
+
+    def getNames(self, file, ids):
+        absent = []
+        with open(file, "r") as f:
+            lines = f.readlines()
+            for i in ids:
+                for j in lines:
+                    if j.strip().split(",")[0] == i:
+                        absent.append([j.strip().split(",")[0], j.strip().split(",")[1], j.strip().split(",")[3]])
         return absent
+
 p = ProcessID()
+        #file you scanned barcodes into     file you want to store the ID# (no noise) into
 p.removeBackground("AttendanceSheet.txt", "ProcessID.txt")
-absent = p.compareWithRoster("ProcessID.txt", "AttendanceSheet.txt")
+                                                #main roster
+absent = p.compareWithRoster("ProcessID.txt", "mockSheet.txt")
+for i in absent:
+    print(i)
 
